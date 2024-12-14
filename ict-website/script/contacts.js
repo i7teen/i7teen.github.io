c5c117d2d194ab69f1097e846df7e3453343610e
i7 <91188982+i7teen@users.noreply.github.com>
2024-12-14 14:20:09 +0800

    Add files via upload

---
+++ ict-website/script/contacts.js
@@ -0,0 +1,9 @@
+
+const form = document.querySelector(".contact-form");
+
+function showform(){
+    form.style.transform = "translateY(0)"
+}
+function closeform(){
+    form.style.transform = "translateY(-100%)"
+}
