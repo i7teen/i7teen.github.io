f8dffcd02c8021b8cc6abd6f7100a15b0eb17037
i7 <91188982+i7teen@users.noreply.github.com>
2024-12-14 14:18:25 +0800

    Delete ict-website directory

--- ict-website/script/intro.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("DOMContentLoaded", (e) => {
-    gsap.to('.images', {duration: 1, opacity: 1, stagger: .15});
-    gsap.to('.marquee', {duration: 1, translateY: 0, delay: 1.4});
-    gsap.to('.circle-text-container', {duration: 1, translateY: 0, delay: 1.4});
-    gsap.to('.pic-container', {duration: 1, translateY: 0, delay: 1.4});
-})
